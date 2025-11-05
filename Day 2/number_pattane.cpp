bcc2dfd906b0fcdd84ce8b42da4c7864069fce3e
Roshan Gupta <roshangupta112113@gmail.com>
2025-11-05 13:29:28 +0530

    Add number pattern printing program in C++

---
+++ Day 2/number_pattane.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout << "Enter a number: ";
+	cin >> x;
+	 
+	 
+	for (int i = 1; i <= x; i++){
+		for (int j = 1; j <= x; j++)
+		{
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
